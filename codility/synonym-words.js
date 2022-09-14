0e67474f9a8aaf1ca568b534f1441ab8caec23f6
Zishan Ansari <zishana@prdxn.com>
2022-09-14 19:22:11 +0530

    feat: synonyms program

---
+++ codility/synonym-words.js
@@ -0,0 +1,94 @@
+const fs = require('fs');
+
+const filePath = './codility/files/example.in';
+
+const fileData = fs.readFileSync(filePath, 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  return data;
+});
+
+const lines = fileData.split('\n').filter((item) => item);
+const testCases = lines.shift() * 1;
+let cases = [];
+let linesObject = {};
+let counter = 0;
+let dictionary = {};
+let output = [];
+
+lines.forEach((item) => {
+  if (!isNaN(item)) {
+    lineNum = item;
+    cases.push(item);
+    linesObject[lineNum] = [];
+  } else {
+    linesObject[lineNum] =
+      linesObject[lineNum].length === 0
+        ? [item]
+        : [...linesObject[lineNum], item];
+  }
+});
+
+const synonyms = (lines) => {
+  let tempDictionary = {};
+  for (let i = 0; i < lines.length; i++) {
+    let [first, second] = lines[i].split(' ');
+    first = first.toLowerCase();
+    second = second.toLowerCase();
+
+    if (!tempDictionary[first]) {
+      tempDictionary[first] = [second];
+    } else {
+      tempDictionary[first] = [...tempDictionary[first], second];
+      const item = tempDictionary[first][0];
+      if (tempDictionary[item].includes(first)) {
+        tempDictionary[item] = [...tempDictionary[item], second];
+      }
+    }
+
+    if (!tempDictionary[second]) {
+      tempDictionary[second] = [first];
+    } else {
+      tempDictionary[second] = [...tempDictionary[second], first];
+      const item = tempDictionary[second][0];
+      if (tempDictionary[item].includes(second)) {
+        tempDictionary[item] = [...tempDictionary[item], first];
+      }
+    }
+  }
+
+  return tempDictionary;
+};
+
+const resolveQuery = (lines, dict) => {
+  for (let i = 0; i < lines.length; i++) {
+    let [first, second] = lines[i].split(' ');
+    first = first.toLocaleLowerCase();
+    second = second.toLocaleLowerCase();
+    if (
+      dict[first]?.includes(second) ||
+      dict[second]?.includes(first) ||
+      first === second
+    ) {
+      output.push('synonyms');
+    } else {
+      output.push('different');
+    }
+  }
+};
+
+for (let index = 0; index < cases.length; index++) {
+  item = linesObject[cases[index]];
+  if (index === 0 || index % 2 === 0) {
+    dictionary = synonyms(item);
+  } else {
+    resolveQuery(item, dictionary);
+    counter++;
+    if (counter === testCases) break;
+  }
+}
+
+console.log(output.join('\n'));
