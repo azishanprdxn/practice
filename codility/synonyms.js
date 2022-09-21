12f80924922e5730e906b727791758aa43531852
Zishan Ansari <zishana@prdxn.com>
2022-09-21 13:00:33 +0530

    feat: Update synonym program

--- codility/synonyms.js
+++ codility/synonyms.js
@@ -20,26 +20,20 @@ let counter = 0;
 let dictionary = {};
 let output = [];
 
-lines.forEach((item) => {
-  if (!isNaN(item)) {
-    lineNum = item;
-    cases.push(item);
+let iter = 0;
+for (let i = 0; i < lines.length; i++) {
+  if (!isNaN(lines[i])) {
+    lineNum = `${iter} - ${lines[i]}`;
+    cases.push(`${iter} - ${lines[i]}`);
     linesObject[lineNum] = [];
+    iter++;
   } else {
     linesObject[lineNum] =
       linesObject[lineNum].length === 0
-        ? [item]
-        : [...linesObject[lineNum], item];
+        ? [lines[i]]
+        : [...linesObject[lineNum], lines[i]];
   }
-});
-
-obj = {
-  first: 1,
-  second: 0,
-  third: 1,
-  fourth: 2,
-  fifth: 1,
-};
+}
 
 const createDictionary = (lines, index) => {
   let tempDictionary = {};
@@ -49,48 +43,43 @@ const createDictionary = (lines, index) => {
     first = first.toLowerCase();
     second = second.toLowerCase();
 
-    if (index === 0) {
-      if (first === second) {
-      } else if (first in tempDictionary && second in tempDictionary) {
-        let dkeyOld = tempDictionary[first];
-        let dvalueOld = tempDictionary[second];
-        for (const [key, value] of Object.entries(tempDictionary)) {
-          if (value === dvalueOld || value === dkeyOld) {
-            tempDictionary[key] = iterator;
-          }
+    if (first === second) {
+    } else if (first in tempDictionary && second in tempDictionary) {
+      let dKeyOld = tempDictionary[first];
+      let dValueOld = tempDictionary[second];
+      for (const [key, value] of Object.entries(tempDictionary)) {
+        if (value === dValueOld || value === dKeyOld) {
+          tempDictionary[key] = iterator;
         }
-        iterator++;
-      } else if (first in tempDictionary) {
-        if (!(second in tempDictionary)) {
-          tempDictionary[second] = tempDictionary[first];
-        }
-      } else if (second in tempDictionary) {
-        if (!(first in tempDictionary)) {
-          tempDictionary[first] = tempDictionary[second];
-        }
-      } else {
-        tempDictionary[first] = iterator;
-        tempDictionary[second] = iterator;
-
-        iterator++;
       }
+      iterator++;
+    } else if (first in tempDictionary) {
+      if (!(second in tempDictionary)) {
+        tempDictionary[second] = tempDictionary[first];
+      }
+    } else if (second in tempDictionary) {
+      if (!(first in tempDictionary)) {
+        tempDictionary[first] = tempDictionary[second];
+      }
+    } else {
+      tempDictionary[first] = iterator;
+      tempDictionary[second] = iterator;
+
+      iterator++;
     }
   }
 
-  // console.log(tempDictionary);
   return tempDictionary;
 };
 
 const resolveQuery = (lines, dict, index) => {
-  // if (index === 1) {
-  //   console.log('dict', dict);
-  // }
   for (let i = 0; i < lines.length; i++) {
     let [first, second] = lines[i].split(' ');
     first = first.toLowerCase();
     second = second.toLowerCase();
+
     if (dict && first in dict && second in dict) {
-      if (dict[first] === dict[second]) {
+      if (first in dict === second in dict) {
         output.push('synonyms');
       } else {
         output.push('different');
@@ -110,13 +99,12 @@ for (let index = 0; index < cases.length; index++) {
   if (index === 0 || index % 2 === 0) {
     dictionary = createDictionary(item, index);
   } else {
-    if (index === 1) {
-      resolveQuery(item, dictionary, index);
-    }
+    resolveQuery(item, dictionary, index);
     counter++;
+
     if (counter === testCases) break;
   }
 }
 
-// fs.writeFileSync(outputPath, output.join('\n'));
-console.log(output.join('\n'));
+fs.writeFileSync(outputPath, output.join('\n'));
+// console.log(output.join('\n'));
